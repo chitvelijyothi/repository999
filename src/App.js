1a36a70f9027098affa782b5638d9d627ddd125b
chitvelijyothi <jyothichitveli@gmail.com>
2024-04-22 15:51:27 +0530

    added heading

--- src/App.js
+++ src/App.js
@@ -4,36 +4,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <form>
-        <h1>FORM</h1>
-        <div>
-        <label>d</label>
-        <input></input>
-        </div>
-        <div>
-        <label>c</label>
-        <input></input>
-        </div>
-        <div>
-        <label>v</label>
-        <input></input>
-        </div>
-        <div>
-        <label>j</label>
-        <input></input>
-        </div>
-        <div>
-        <label>e</label>
-        <input></input>
-        </div>
-        <div>
-        <label>t</label>
-        <input></input>
-        </div>
-        <div>
-          <button>submit</button>
-        </div>
-      </form>
+     <h1>Zomato</h1>
+        
     </div>
   );
 }
