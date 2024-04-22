c1ab71e9816a68e9595f61fef8c90a5ee56b5450
chitvelijyothi <jyothichitveli@gmail.com>
2024-04-22 15:25:45 +0530

    added initial project files

---
+++ src/App.js
@@ -0,0 +1,41 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <form>
+        <h1>FORM</h1>
+        <div>
+        <label>d</label>
+        <input></input>
+        </div>
+        <div>
+        <label>c</label>
+        <input></input>
+        </div>
+        <div>
+        <label>v</label>
+        <input></input>
+        </div>
+        <div>
+        <label>j</label>
+        <input></input>
+        </div>
+        <div>
+        <label>e</label>
+        <input></input>
+        </div>
+        <div>
+        <label>t</label>
+        <input></input>
+        </div>
+        <div>
+          <button>submit</button>
+        </div>
+      </form>
+    </div>
+  );
+}
+
+export default App;
