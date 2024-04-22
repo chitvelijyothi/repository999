b04d10039cdf0a5ff712268b48853e0023cf6f96
chitvelijyothi <jyothichitveli@gmail.com>
2024-04-22 18:34:19 +0530

    added some files

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-
+import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-     <h1>Zomato</h1>
-        
+    <h1>Zomato</h1>
     </div>
   );
 }
