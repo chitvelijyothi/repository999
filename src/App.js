18e8936ed037f97134ac492b3e580f7a4b7de991
chitvelijyothi <167517764+chitvelijyothi@users.noreply.github.com>
2024-04-23 12:07:44 +0530

    Update App.js
    added paragraph

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ function App() {
   return (
     <div className="App">
     <h1>Zomato</h1>
-    <p>Zomato was founded as FoodieBay in 2008 by Deepinder Goyal and Pankaj Chaddah who worked for Bain & Company. The website started as a restaurant-listing-and-recommendation portal. They renamed the company Zomato in 2010 as they were unsure if they would "just stick to food" and also to avoid a potential naming conflict with eBay</p>
+    <p>Zomato is an Indian multinational restaurant aggregator and food delivery company </p>
+  
     </div>
   );
 }
